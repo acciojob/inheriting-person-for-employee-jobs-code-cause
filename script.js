b12522c280c4773ec2c0e5810a295996eb198328
Ashish Pandey <writeashish22@gmail.com>
2025-03-23 09:48:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-// complete this js code
-function Person(name, age) {}
+// // complete this js code
+// function Person(name, age) {}
 
-function Employee(name, age, jobTitle) {}
+// function Employee(name, age, jobTitle) {}
+
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
+// Define the Person class
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+}
+
+// Define the Employee class that inherits from Person
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); // Call the parent class constructor
+        this.jobTitle = jobTitle;
+    }
+    
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+}
 
 // Do not change code below this line
 window.Person = Person;
